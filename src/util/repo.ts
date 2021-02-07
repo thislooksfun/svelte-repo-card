a0652eedab22a42d46f57d6b63925ee5c2c574d1
thislooksfun <thislooksfun@repbot.org>
2021-02-07 16:01:29 -0600

    refactor: split repo transformation into own function

--- src/util/repo.ts
+++ src/util/repo.ts
@@ -1,9 +1,21 @@
 import get from "./get";
 import getColors from "./colors";
 
-export interface Repo {
-  colors: any;
+export interface RawRepo {
+  name: string;
+  html_url: string;
+  description?: string;
+  language?: string;
+  fork: boolean;
+  source?: {
+    full_name: string;
+    html_url: string;
+  };
+  stargazers_count: number;
+  forks: number;
+}
 
+export interface Repo {
   name: string;
   url: string;
   isFork: boolean;
@@ -16,24 +28,31 @@ export interface Repo {
   forks: number;
 }
 
-type AnyObject = { [key: string]: any };
-export default async function getRepo(slug: string): Promise<Repo> {
-  const repo: AnyObject = await get(`https://api.github.com/repos/${slug}`);
+async function transform(repo: RawRepo): Promise<Repo> {
   const colors = await getColors();
 
-  // Extract out only the props we need, and give them better names.
-  return {
-    colors,
-
+  let out: Repo = {
     name: repo.name,
     url: repo.html_url,
-    description: repo.description,
-    language: repo.language,
-    langColor: repo.language && colors[repo.language].color,
     isFork: repo.fork,
-    sourceUrl: repo.fork && repo.source.html_url,
-    sourceName: repo.fork && repo.source.full_name,
     stars: repo.stargazers_count,
     forks: repo.forks,
   };
+
+  if (repo.description != null) out.description = repo.description;
+  if (repo.source != null) {
+    out.sourceName = repo.source.full_name;
+    out.sourceUrl = repo.source.html_url;
+  }
+  if (repo.language != null) {
+    out.language = repo.language;
+    out.langColor = colors[repo.language].color;
+  }
+
+  return out;
+}
+
+export default async function getRepo(slug: string): Promise<Repo> {
+  const repo: RawRepo = await get(`https://api.github.com/repos/${slug}`);
+  return await transform(repo);
 }
